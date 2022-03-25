1a3077ceb8deb7a9fbe19529b9bbee29ac48dcf6
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2022-03-25 12:53:24 -0600

    Sort/todo on properties views.py

--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from properties.models import Property
 from properties.serializers import PropertySerializer
 
@@ -7,3 +8,8 @@ from properties.serializers import PropertySerializer
 class PropertyViewSet(ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+
+    # TODO: Sort properties by landlord id
+    @action(detail=False, methods=["GET"])
+    def sort(self, request):
+        pass
